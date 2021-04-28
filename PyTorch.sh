fa5144296fda01ab1968ae3cfdec7d6f72f96498
DeepAbyss-s <68440516+DeepAbyss-s@users.noreply.github.com>
2021-04-28 18:24:02 +0900

    Update PyTorch.sh

--- PyTorch.sh
+++ PyTorch.sh
@@ -1,25 +1,3 @@
-sudo apt-get update
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
-libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-xz-utils tk-dev
+wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
 
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
-
-source ~/.bash_profile
-# 설치 가능한 버전은 pyenv install --list 명령어로 확인가능
-pyenv install 3.7.1
-
-git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
-
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
-
-source ~/.bash_profile
-
-pyenv virtualenv 3.7.1 py37
-
- pyenv activate py37
- 
- pip install torch torchvision
+sudo sh ./Anaconda3-2020.11-Linux-x86_64.sh
